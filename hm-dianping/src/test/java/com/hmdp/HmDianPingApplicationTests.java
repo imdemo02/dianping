f9e2077606299f56a0b3c7218b0fec5854500672
imdemo <imdemo@qq.com>
2023-03-20 13:53:25 +0800

    实现附近商铺功能

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,14 +1,25 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -19,6 +30,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -46,5 +60,31 @@ class HmDianPingApplicationTests {
     @Test
     void testSaveShop() throws InterruptedException {
 //        shopService.saveShop2Redis(1L, 10L);
+    }
+
+    @Test
+    void loadShopData() {
+        //1.查询店铺信息
+        List<Shop> list = shopService.list();
+        //2.将店铺按照typeId分组 id一致的放到一个集合  以typeId为key 集合为value 放到一个map中
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        //3.分批写入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            //3.1获取类型id
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            //3.2获取同类型的店铺数据集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            //3.3写入redis  GEOADD key 经度 维度 member
+            for (Shop shop : value) {
+                //效率较低  每一个店铺都要发一个请求
+                //stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                //先把数据写入到 locations集合中 最后再批量的写入到redis中  提升效率
+                locations.add(new RedisGeoCommands.GeoLocation<>(shop.getId().toString(), new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+
+        }
     }
 }
